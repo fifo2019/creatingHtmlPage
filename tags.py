144bafbe151bcc89f2a5e5fe356956438be990b0
pavel <pavel.dudkov@icloud.com>
2020-04-17 01:27:15 +0400

    17.04.2020
    1. Сделал запуск через терминал
    2. Отладил работу добавления отступов к тегам

--- tags.py
+++ tags.py
@@ -1,40 +1,54 @@
 class Tag:
-    def __init__(self, tag, parent=False, is_single=False, *args, **kwargs):
+    def __init__(self, tag, is_parent=False, is_single=False, *args, **kwargs):
         self.tag = tag
         self.childern = []
         self.text = ""
-        self.parent = parent
+        self.is_parent = is_parent
         self.is_single = is_single
         self.args = args
         self.kwargs = kwargs
 
     def __str__(self):
         if self.kwargs:
-            # TODO: ОТЛАДИТЬ ТУТ - РАБОТАЕТ НЕ КОРРЕКТНО - ЗАТИРАЕТ ВЛОЖЕННЫЙ ТЕКСТ!!!!
-            print(self.kwargs)
+            # Если в теги есть атрибуты
             attrs = []
             for attr in self.kwargs.keys():
                 if attr not in attrs:
-                    attrs.append(f' {attr}="')
+                    # Если атрибута нет в списке добавляем его
+                    attrs.append(f' {attr}="'.replace('_', '-'))
                     if type(self.kwargs.get(attr)) != str:
+                        # Получаем значения атрибута, если не является строкой распаковываем последовательность
                         attrs.append(' '.join(self.kwargs.get(attr)) + '"')
                     else:
+                        # Если строка добавляем к атрибуту
                         attrs.append(self.kwargs.get(attr) + '"')
+            # Собираем строку со всеми атрибутами и значениями для тега
             attrs = "".join(attrs).replace('klass', 'class')
-            element = []
-            for cild in self.childern:
-                element.append(f'\t\t{cild}\n')
-            element = ''.join(element)
-            return f'\t<{self.tag}{attrs}>\n{element}\t\t</{self.tag}>'
-        elif self.parent:
-            element = []
-            for cild in self.childern:
-                element.append(f'\t{cild}\n')
-            element = ''.join(element)
-            return f'\t<{self.tag}>\n{element}\t</{self.tag}>'
+            if self.is_single:
+                # Если тег одиночный
+                return f'\t<{self.tag}{attrs}/>'
+            if self.is_parent:
+                # Если тег является родителем
+                element = []
+                for cild in self.childern:
+                    element.append(f'\t{cild}\n')
+                element = '\t'.join(element)
+                return f'\t<{self.tag}{attrs}>\n\t{element}\t\t</{self.tag}>'
+            else:
+                # Если тег вложенный и с атрибутами
+                return f'\t<{self.tag}{attrs}>{self.text}</{self.tag}>'
         elif self.is_single:
-            return f'\t<{self.tag}>'
+            # Если тег одиночный и без атрибутов
+            return f'\t<{self.tag}/>'
+        elif self.is_parent:
+            # Если тег является родителем и без атрибутов
+            element = []
+            for cild in self.childern:
+                element.append(f'{cild}\n')
+            element = '\t'.join(element)
+            return f'\t<{self.tag}>\n\t{element}\t</{self.tag}>'
         else:
+            # Если тег вложенный и без атрибутов
             return f"\t<{self.tag}>{self.text}</{self.tag}>"
 
     def __iadd__(self, other):
@@ -49,9 +63,9 @@ class Tag:
 
 
 class HTML(Tag):
-    def __init__(self, tag='html', output=None):
-        super().__init__(tag)
-        self.output = output
+    def __init__(self, tag='html', *args, **kwargs):
+        super().__init__(tag, *args, **kwargs)
+        self.output = self.kwargs.get('output')
 
     def __str__(self):
         element = [f'<{self.tag}>\n']
@@ -65,7 +79,6 @@ class HTML(Tag):
             print(self)
         elif self.output is not None:
             try:
-                # TODO: Убрать перезапись  на х
                 with open(f'{self.output}.html', 'w', encoding='utf-8') as file:
                     element = []
                     for cild in self.childern:
@@ -79,6 +92,6 @@ class HTML(Tag):
 
 
 class TopLevelTag(Tag):
-    def __init__(self, tag, parent=True, *args, **kwargs):
-        super().__init__(tag, parent, *args, **kwargs)
+    def __init__(self, tag, is_parent=True, *args, **kwargs):
+        super().__init__(tag, is_parent, *args, **kwargs)
 
